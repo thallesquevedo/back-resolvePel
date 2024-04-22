49594cdfcbae8509f93a1e2d1e31cc649e2aa7c8
thallesquevedo <bernardoquevedo@gmail.com>
2024-04-22 13:50:05 -0300

    init configs

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,17 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+import 'dotenv/config';
+const { DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_DATABASE } = process.env;
+
+export const typeormConfig: DataSourceOptions = {
+  type: 'postgres',
+  host: DB_HOST,
+  port: parseInt(DB_PORT),
+  username: DB_USERNAME,
+  password: DB_PASSWORD,
+  database: DB_DATABASE,
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  migrations: [__dirname + '/../../migrations/*.{js,ts}'],
+  synchronize: false,
+};
+
+export const connectionSource = new DataSource(typeormConfig);
