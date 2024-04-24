0017141ad8179c4ce0909846058b638334d95c9b
thallesquevedo <bernardoquevedo@gmail.com>
2024-04-24 17:06:52 -0300

    chore(auth): create module, service, controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeormConfig } from './config/typeorm.config';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeormConfig), UserModule],
+  imports: [TypeOrmModule.forRoot(typeormConfig), UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
